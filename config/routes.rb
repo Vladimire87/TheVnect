722cfc21c5ba2d2aa6931e6b5855df40423ddf37
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-11 20:51:46 +0400

    generate divise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  authenticated :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
 end
