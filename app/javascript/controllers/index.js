ccfc5df20fe3ea2344d41a04fac5552612024054
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-12 16:29:43 +0400

    add log out to home index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application'
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from './hello_controller'
+application.register('hello', HelloController)
+
+import Dropdown from 'stimulus-dropdown'
+application.register('dropdown', Dropdown)
+
+import PasswordVisibility from 'stimulus-password-visibility'
+application.register('password-visibility', PasswordVisibility)
