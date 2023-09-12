61229dc21d110a32192696e17a0e5410aa663279
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-12 12:44:47 +0400

    add login merakiui for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   devise_for :users
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+  end
   authenticated :user do
     root to: 'home#index', as: :authenticated_root
   end
-  root to: redirect('/users/sign_in')
+  root to: redirect('/login')
 end
