3996b8f6c699db9bb3271c32ab3f77476b8d65eb
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-12 13:52:43 +0400

    add scope for devise sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/login' => 'devise/sessions#new'
+    get '/register' => 'devise/registrations#new'
     get '/logout' => 'devise/sessions#destroy'
   end
   authenticated :user do
