28ea126c8ffd2020c67c2679ea16940117ac6181
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-21 22:06:26 +0400

    add user tos box

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable
   validates :first_name, :last_name, :email, presence: true
+  validates :tos_agreement, acceptance: { allow_nil: false, on: :create }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-  get '/terms_and_conditions', to: 'terms#show'
   devise_for :users
+
   devise_scope :user do
     get '/login' => 'devise/sessions#new'
     get '/register' => 'devise/registrations#new'
     get '/logout' => 'devise/sessions#destroy'
   end
+
   authenticated :user do
     root to: 'home#index', as: :authenticated_root
   end
+
   root to: redirect('/login')
+
+  get '/terms_and_conditions', to: 'terms#show'
 end
