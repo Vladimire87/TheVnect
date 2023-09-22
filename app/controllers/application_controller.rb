97768d4e644e25c05c29d4042a5ae965838e5ca6
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-22 17:01:27 +0400

    add strong parameter for edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name tos_agreement])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[first_name last_name email password password_confirmation
+                                               current_password])
   end
 end
