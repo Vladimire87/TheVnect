8f7dc01f490ab2afc78017e1a0d8caf5eb95f1ff
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-09-27 19:54:59 +0400

    add edit option with out password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   as :user do
     get 'users/profile', to: 'devise/registrations#edit', as: :user_root
   end
