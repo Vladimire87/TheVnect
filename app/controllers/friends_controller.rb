29e3143c393bdc6106200e63c13c318372a9da3f
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-10-04 17:44:32 +0400

    add helper for active link

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class FriendsController < ApplicationController
-  def show
-  end
+  def show; end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,16 @@ module ApplicationHelper
   def pending_requests
     current_user.pending_requests.includes(followable: { avatar_attachment: :blob })
   end
-end
 
-# user: vlad
-# USE eager loading detected
-#   Followability::Relationship => [:followable]
-#   Add to your query: .includes([:followable])
-# Call stack
-#   /Users/vlad/Projects/TheVnect/app/views/friends/_pending_request_card.html.erb:5:in `block in _app_views_friends__pending_request_card_html_erb__3051409894390280214_55020'
-#   /Users/vlad/Projects/TheVnect/app/views/friends/_pending_request_card.html.erb:2:in `_app_views_friends__pending_request_card_html_erb__3051409894390280214_55020'
-#   /Users/vlad/Projects/TheVnect/app/views/friends/show.html.erb:4:in `_app_views_friends_show_html_erb___4019434777848760638_55000'
+  def active_link_to(text = nil, path = nil, **options, &)
+    link = block_given? ? text : path
+
+    options[:class] = class_names(options[:class], 'bg-gray-900 text-white') if current_page?(link)
+
+    if block_given?
+      link_to(link, options, &)
+    else
+      link_to text, path, options
+    end
+  end
+end
