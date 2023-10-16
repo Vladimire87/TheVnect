022cb13ef9cace3294dc7eff89857b33be0f664a
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-10-16 16:17:25 +0400

    update hoem controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     # @users = User.includes(avatar_attachment: :blob).all
     # @users = User.where.not(id: current_user).includes(avatar_attachment: :blob)
     # @posts = Post.includes([:user]).order(created_at: :DESC).all
-    @timeline = Post.where(user_id: current_user.id).includes(user: [avatar_attachment: :blob]).or(Post.where(user_id: current_user.following.ids)).order(created_at: :desc)
+    @timeline = Post.where(user_id: current_user.id)
+                    .includes(user: [avatar_attachment: :blob])
+                    .or(Post.where(user_id: current_user.following.ids))
+                    .order(created_at: :desc)
   end
 end
